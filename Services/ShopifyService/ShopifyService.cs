a8e93cb5ad099c832d70442efc8e42708cc410cb
kwidzinski_nanoleq <kwidzinski@nanoleq.com>
2024-06-27 15:57:26 +0200

    add IConfiguration import

--- Services/ShopifyService/ShopifyService.cs
+++ Services/ShopifyService/ShopifyService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SOPManagement.Services.ShopifyService.Helpers;
+using Microsoft.Extensions.Configuration;
 
 namespace SOPManagement.Services.ShopifyService
 {
